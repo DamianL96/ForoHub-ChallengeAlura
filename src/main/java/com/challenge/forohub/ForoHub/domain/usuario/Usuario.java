53b49ba27be3038b8e1c2fd51bcc84d38c260e9b
Damian Lambrecht <damianlambrecht@gmail.com>
2025-07-28 19:58:02 -0300

    JPA usuario

--- src/main/java/com/challenge/forohub/ForoHub/domain/usuario/Usuario.java
+++ src/main/java/com/challenge/forohub/ForoHub/domain/usuario/Usuario.java
@@ -1,18 +1,26 @@
 package com.challenge.forohub.ForoHub.domain.usuario;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name="usuarios")
+@Entity(name="Usuario")
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of ="id")
 
 public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nombre;
-
     private String email;
-
     private String password;
+
 }
