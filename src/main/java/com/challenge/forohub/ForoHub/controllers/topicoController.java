54b049196954baf500d08e34ce551fee812fea67
Damian Lambrecht <damianlambrecht@gmail.com>
2025-07-28 14:00:07 -0300

    listar topicos finalizado

--- src/main/java/com/challenge/forohub/ForoHub/controllers/topicoController.java
+++ src/main/java/com/challenge/forohub/ForoHub/controllers/topicoController.java
@@ -1,18 +1,14 @@
 package com.challenge.forohub.ForoHub.controllers;
 
-import com.challenge.forohub.ForoHub.domain.topico.DatosRegistroTopico;
-import com.challenge.forohub.ForoHub.domain.topico.RegistroDeTopicos;
-import com.challenge.forohub.ForoHub.domain.topico.Topico;
-import com.challenge.forohub.ForoHub.domain.topico.TopicoRepository;
+import com.challenge.forohub.ForoHub.domain.topico.*;
 import jakarta.validation.Valid;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/topicos")
 @RestController
@@ -31,5 +27,12 @@ public class topicoController {
         registro.registrarTopico(datos); //validamos
 
         return ResponseEntity.ok("Topico registrado con existo");
+    }
+
+    @GetMapping
+    public ResponseEntity listar(){
+        var listado = topicoRepository.findAll()
+                .stream().map(DatosDetalleTopico::new).toList();
+        return ResponseEntity.ok(listado);
     }
 }
--- src/main/java/com/challenge/forohub/ForoHub/infra/security/SecurityConfigurations.java
+++ src/main/java/com/challenge/forohub/ForoHub/infra/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package com.challenge.forohub.ForoHub.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,7 +16,8 @@ public class SecurityConfigurations {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable())
             .authorizeHttpRequests( (requests)-> requests
-                .requestMatchers("/topicos").permitAll()
+                .requestMatchers(HttpMethod.POST,"/topicos").permitAll()
+                .requestMatchers(HttpMethod.GET,"/topicos").permitAll()
                 .anyRequest().authenticated())
             .httpBasic(Customizer.withDefaults());
 
